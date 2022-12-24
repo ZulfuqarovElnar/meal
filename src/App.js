5b4918de60d626e3f70200326f02eaa336b14ffc
Elnar <elnarzulfuqarli2001@gmail.com>
2022-12-24 12:24:53 +0400

    Yemeklerin novlerini qeyd edildiyi bolme Navbara eklendi ve bezi deyişikler edildi

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import {Routes, Route} from "react-router-dom";
 
 import Home from './routes/Home'
+import Meal from "./routes/Meal"
 
 function App() {
   return (
     <Routes>
     <Route path={'/'} element={<Home />}></Route>
+    <Route path={'/meal'} element={<Meal />}></Route>
   </Routes>
   );
 }
---
+++ src/routes/Meal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Table  from '../components/Table';
+
+import Navbar from '../components/Navbar';
+
+
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <Table />
+    </div>
+  )
+}
+
+export default Home
